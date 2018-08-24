a0c18f91289c143d0e73950ab2fd0080ed0b63a3
julioc98 <julioc.santos98@gmail.com>
2018-08-24 20:36:20 -0300

    Base

--- course/course.go
+++ course/course.go
@@ -1,12 +1,64 @@
 package course
 
-import "github.com/jinzhu/gorm"
+import (
+	"github.com/fiscaluno/pandorabox/db"
+)
 
-// Course is a Human
+// Course is a Entity
 type Course struct {
-	gorm.Model
-	Name string `json:"name"`
+	Name          string  `json:"course_name"`
+	Type          string  `json:"course_type"`
+	AverageRating float64 `json:"course_average_rating"`
+	RatedByCount  int     `json:"course_rated_by_count"`
 }
 
-// Courses is a slice for Course
-// var Courses []Course
+// Entity is a course
+type Entity struct {
+	Course
+	db.CommonModelFields
+}
+
+// Entitys is Entity slice
+type Entitys []Entity
+
+// GetAll Entitys
+func GetAll() Entitys {
+	db := db.Conn()
+	defer db.Close()
+	var entitys Entitys
+	db.Find(&entitys)
+	return entitys
+}
+
+// Save a Entity
+func (entity Entity) Save() (Entity, error) {
+	db := db.Conn()
+	defer db.Close()
+
+	db.Create(&entity)
+
+	return entity, nil
+}
+
+// GetByID a Entity
+func GetByID(id int) Entity {
+	db := db.Conn()
+	defer db.Close()
+
+	var entity Entity
+
+	db.Find(&entity, id)
+
+	return entity
+}
+
+// GetByQuery a Entity
+func GetByQuery(query string, value interface{}) Entitys {
+	db := db.Conn()
+	defer db.Close()
+
+	var entitys Entitys
+
+	db.Find(&entitys, query, value)
+	return entitys
+}
--- course/routes.go
+++ course/routes.go
@@ -1,12 +1,13 @@
 package course
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/gorilla/mux"
+)
 
 // SetRoutes add routes from course
 func SetRoutes(r *mux.Router) {
 	r.HandleFunc("", Add).Methods("POST")
-	r.HandleFunc("", GetAll).Methods("GET")
-	r.HandleFunc("/names/{name}", FindByName).Methods("GET")
+	r.HandleFunc("", FindAll).Methods("GET")
 	r.HandleFunc("/{id}", FindByID).Methods("GET")
 	r.HandleFunc("/{id}", DeleteByID).Methods("DELETE")
 	r.HandleFunc("/{id}", UpdateByID).Methods("PUT")
